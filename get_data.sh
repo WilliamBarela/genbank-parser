e9764d2f1158b68820dbfd177c4178fa9dc3cd86
William Barela <william.barela.dev@gmail.com>
2018-04-17 09:31:49 -0500

    add get_data.sh to wget databases from genbank

---
+++ get_data.sh
@@ -0,0 +1,5 @@
+# getting GenBank data from FTP server
+cd ..
+wget -r 'ftp://ftp.ncbi.nlm.nih.gov/genbank/gbmam*'   # by default, wget retries 20 times: --tries=20; max: --tries=inf
+cd ftp.ncbi.nlm.nih.gov/genbank
+for file in $(ls ./); do gunzip $file; done;
