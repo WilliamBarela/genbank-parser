9a1b55b442309625967680e1d236128dbd285d84
William Barela <william.barela.dev@gmail.com>
2018-04-17 08:42:22 -0500

    add additional fields to check for ttum numbers

--- parser.py
+++ parser.py
@@ -3,6 +3,22 @@ import os
 import csv
 import re
 
+def default_search():
+    """all of these searches are case insensitive because of reg_row_parser has re.IGNORECASE"""
+    regex=[
+            r'TK \d+',      # TK [NUMBER]
+            r'TK\d+',       # TK[NUMBER]
+            r'TTU \d+',     # TTU [NUMBER]
+            r'TTU\d+',      # TTU[NUMBER]
+            r'TTUM \d+',    # TTUM [NUMBER]
+            r'TTUM\d+',     # TTUM[NUMBER]
+            r'TTU M \d+',   # TTU M [NUMBER]
+            r'TTU M\d+',    # TTU M[NUMBER]
+            r'TTU-M \d+',   # TTU-M [NUMBER]
+            r'TTU-M\d+'     # TTU-M[NUMBER]
+            ]
+    return regex
+
 def seq_to_dict(filename):
     file = SeqIO.parse(filename, 'gb')
     return [record for record in file]
@@ -25,18 +41,32 @@ def reg_row_parser(regex, filename):
                       record.annotations['accessions'][0]               # accession number
                      ]
 
+            # tk/ttu/etc regex match in description 
             if re.search(pattern, record.description, re.IGNORECASE):
-                fields.append( re.search(pattern, record.description, re.IGNORECASE).group(0) )                 # tk/ttu/etc regex match in description 
+                fields.append( re.search(pattern, record.description, re.IGNORECASE).group(0) )
                 rows.append(fields)
                 continue
+            # tk/ttu/etc regex match in source 
             elif re.search(pattern, record.annotations['source'], re.IGNORECASE):
-                fields.append( re.search(pattern, record.annotations['source'], re.IGNORECASE).group(0) )       # tk/ttu/etc regex match in source 
+                fields.append( re.search(pattern, record.annotations['source'], re.IGNORECASE).group(0) )
                 rows.append(fields)
                 continue
+            # tk/ttu/etc regex match in organism 
             elif re.search(pattern, record.annotations['organism'], re.IGNORECASE):
-                fields.append( re.search(pattern, record.annotations['organism'], re.IGNORECASE).group(0) )     # tk/ttu/etc regex match in organism 
+                fields.append( re.search(pattern, record.annotations['organism'], re.IGNORECASE).group(0) )
                 rows.append(fields)
                 continue
+            # tk/ttu/etc regex match in features organsim if it exists
+            elif 'organism' in record.features[0].qualifiers and re.search(pattern, record.features[0].qualifiers['organism'][0], re.IGNORECASE):
+                fields.append( re.search(pattern, record.features[0].qualifiers['organism'][0], re.IGNORECASE).group(0) )
+                rows.append(fields)
+                continue
+            # tk/ttu/etc regex match in features specimen_voucher if it exists
+            elif 'specimen_voucher' in record.features[0].qualifiers and re.search(pattern, record.features[0].qualifiers['specimen_voucher'][0], re.IGNORECASE):
+                fields.append( re.search(pattern, record.features[0].qualifiers['specimen_voucher'][0], re.IGNORECASE).group(0) )
+                rows.append(fields)
+                continue
+
     return rows
 
 def save_to_csv(regex, input_filename, output_filename):
@@ -50,22 +80,6 @@ def save_to_csv(regex, input_filename, output_filename):
     finally:
         file.close()
     return None 
-
-def default_search():
-    """all of these searches are case insensitive because of reg_row_parser has re.IGNORECASE"""
-    regex=[
-            r'TK \d+',      # TK [NUMBER]
-            r'TK\d+',       # TK[NUMBER]
-            r'TTU \d+',     # TTU [NUMBER]
-            r'TTU\d+',      # TTU[NUMBER]
-            r'TTUM \d+',    # TTUM [NUMBER]
-            r'TTUM\d+',     # TTUM[NUMBER]
-            r'TTU M \d+',   # TTU M [NUMBER]
-            r'TTU M\d+',    # TTU M[NUMBER]
-            r'TTU-M \d+',   # TTU-M [NUMBER]
-            r'TTU-M\d+'     # TTU-M[NUMBER]
-            ]
-    return regex
 
 def reaper(regex=default_search(),output_file="matches.csv"):
     """usage: reaper([r'TK\d+', r'TM\d+', r'TK \d+'])"""
